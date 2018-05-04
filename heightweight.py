b44adf2167695abaa6d16511a18a356907074cd4
kahjuay <kahjuay@gmail.com>
2018-05-04 11:34:12 +0800

    First commit of Python files

---
+++ heightweight.py
@@ -0,0 +1,7 @@
+# Height and Weight
+
+height = 1.79
+weight = 74.2
+bmi = weight / height ** 2
+print(bmi)
+print(type(bmi))
---
+++ test.py
@@ -0,0 +1,46 @@
+'''
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years?
+print (100 * 1.1 ** 7)
+
+#print(dir())
+
+#Lists
+lista = [1, 3, 4, 2]
+listb = [[1, 2, 3], [4, 5, 7]]
+listc = [1 + 2, "a" * 5, 3]
+
+
+print(lista)
+print(listb)
+print(listc)
+'''
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+
+print ("This is the end of the file")
