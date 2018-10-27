508c89b2af1d07fcd604aef1a960ac53c9262c89
Lee Kah Juay <kahjuay@gmail.com>
2018-10-27 18:32:26 +0800

    Adding For Loops examples

---
+++ ForLoops.py
@@ -0,0 +1,15 @@
+for i in [5,4,3,2,1]:
+    print (i)
+print ("Blast Off!")
+
+
+line = input ("How many iterations? ")
+for i in range(1,int(line)+1,1):
+    print (i)
+
+friends = ["Joseph", "Glenn", "Sally"]
+
+for friend in friends:
+    print ("Happy New Year: ", friend)
+
+print ("Done!")
