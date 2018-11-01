e81fefd5cba6831134e86c88f68007ba26eeffe9
kahjuay <kahjuay@gmail.com>
2018-11-01 11:45:43 +0800

    Added StringLoop example

---
+++ StringLoop.py
@@ -0,0 +1,15 @@
+fruit = "banana"
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index,letter)
+    index = index + 1
+
+mytext = input("Type something: ")
+index = 0
+
+while index < len(mytext):
+    letter = mytext[index]
+    print(index, letter)
+    index = index +1
+
