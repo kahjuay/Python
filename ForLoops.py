4a0ff597f91588a885fab13c4d5718038c8362a3
kahjuay <kahjuay@gmail.com>
2018-11-08 17:15:52 +0800

    Added len function

--- ForLoops.py
+++ ForLoops.py
@@ -12,4 +12,8 @@ friends = ["Joseph", "Glenn", "Sally"]
 for friend in friends:
     print ("Happy New Year: ", friend)
 
+for i in range(len(friends)):
+    friend = friends[i]
+    print ("Wonderful New Year: ", friend)
+
 print ("Done!")
