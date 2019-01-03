0ee816b141b70408411622d9c7d0280c84597c06
Lee Kah Juay <kahjuay@gmail.com>
2019-01-03 13:50:38 +0800

    Completed XML Extract assignment

--- xmlExtract.py
+++ xmlExtract.py
@@ -4,17 +4,50 @@ import ssl
 
 serviceurl = input("Enter URL: ")
 if len(serviceurl) < 1:
-    serviceurl = "http://py4e-data.dr-chuck.net/comments_42.xml"
+    # serviceurl = "http://py4e-data.dr-chuck.net/comments_42.xml"
+    serviceurl = "http://py4e-data.dr-chuck.net/comments_146682.xml"
 
 from urllib.request import urlopen
 
 with urlopen(serviceurl) as conn:
+    # while True:
     print ("Retrieving", serviceurl)
     url = urllib.request.urlopen(serviceurl)
 
+    data = url.read()
+
+    print ("Retrieved",len(data), "characters")
+    print(data.decode())
+    tree = ET.fromstring(data)
+
+    intTotalCount = int()
+    intTotalCount = 0
+
+    for elem in tree.iter():
+        if elem.tag == "count":
+            print (elem.tag, elem.text)
+            intTotalCount = intTotalCount + int(elem.text)
+    
+    print ("Total Count is", intTotalCount)
+    # comments = tree.findall('comments')
+    # print(comments[0].find('comment').find('count').text)
+    # mycount = comments[0].find('count').text
+    # print(mycount)
+    
+""" 
 tree = ET.parse(url)
 root = tree.getroot()
 
 counts = tree.findall ('.//count')
 
-print ("Retrieved", counts, "characters")
+print("There are",len(counts), "counts")
+
+for child in root:
+    print (child.tag, child.attrib)
+
+for count in root.findall('comments'):
+    mynum = comments.find('count').text
+    print (count)
+    print (mynum)
+ """
+# print ("Retrieved", counts, "characters")
